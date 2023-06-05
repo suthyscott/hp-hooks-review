9e666ac0566b630646c59888bc96eb19edd44785
suthyscott <sutherlandscott21@gmail.com>
2023-06-05 14:26:20 -0600

    finished hooks and props mini review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import ArticleCard from './ArticleCard';
+
+const {REACT_APP_NEWS_API_KEY} = process.env
 
 function App() {
+  const [articles, setArticles] = useState()
+  // const articles = []
+
+  const getArticles = () => {
+    // console.log('hit getArticle')
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${REACT_APP_NEWS_API_KEY}`)
+    .then(res => {
+      // console.log(res.data)
+      setArticles(res.data.articles)
+    })
+    .catch(theseHands => console.log(theseHands))
+  }
+  
+  // useEffect(getArticles, [])
+  useEffect(() => {
+    getArticles()
+  }, [])
+
+  // console.log('Hit App.js')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       App.js
+       <ArticleCard article={articles[0]}/>
+       {/* {articles.map(e => console.log(e))} */}
     </div>
   );
 }
---
+++ src/ArticleCard.js
@@ -0,0 +1,12 @@
+
+
+const ArticleCard = (props) => {
+    console.log(props)
+    return(
+        <div>
+            {props.article.title}
+        </div>
+    )
+}
+
+export default ArticleCard
